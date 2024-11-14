17ef6da87f2f68899b60cd997b6bcfa31fc69518
AlexDudnik <145279680+AlexeyDudnik@users.noreply.github.com>
2024-11-14 10:48:55 +0300

    Create deserializer.cs

---
+++ deserializer.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using System.Xml.Serialization;
+using AnimalLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(Cow));
+        using (FileStream fs = new FileStream("animaldes.xml", FileMode.Open))
+        {
+            Animal deserializedAnimal = (Animal)serializer.Deserialize(fs);
+            Console.WriteLine($"Имя: {deserializedAnimal.Name}, Страна: {deserializedAnimal.Country}, Любимая еда: {deserializedAnimal.GetFavouriteFood()}, Классификация: {deserializedAnimal.GetClassificationAnimal()}");
+        }
+    }
+}
