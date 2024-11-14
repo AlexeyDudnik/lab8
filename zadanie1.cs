99cd39c458a7f4cb88bc412942951fd662be28cb
AlexDudnik <145279680+AlexeyDudnik@users.noreply.github.com>
2024-11-14 12:08:05 +0300

    Update zadanie1.cs

--- zadanie1.cs
+++ zadanie1.cs
@@ -2,11 +2,17 @@ using System;
 using System.IO;
 using System.Xml.Serialization;
 using AnimalLibrary;
-class Program
+class main
 {
     static void Main()
     {
+        Animal cow = new Cow { Name = "Машка", Country = "Россия", HideFromOtherAnimals = false };
         XmlSerializer serializer = new XmlSerializer(typeof(Cow));
+        using (FileStream fs = new FileStream("animaldes.xml", FileMode.Create))
+        {
+            serializer.Serialize(fs, cow);
+        }
+        Console.WriteLine("XML файл с животным создан.");
         using (FileStream fs = new FileStream("animaldes.xml", FileMode.Open))
         {
             Animal deserializedAnimal = (Animal)serializer.Deserialize(fs);
